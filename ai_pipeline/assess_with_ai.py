d0f34b9e91ac8dbd29efb6d98af175a950d75953
sf17490 <sflynn@duck.com>
2025-10-03 22:19:30 +0100

    Correct the env key name in python script

--- ai_pipeline/assess_with_ai.py
+++ ai_pipeline/assess_with_ai.py
@@ -1,11 +1,9 @@
 from openai import OpenAI, APIError, APITimeoutError
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")
+    api_key=os.getenv("OPENAPI_API_KEY")
 )
 
 
